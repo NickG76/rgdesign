edbe8127b9c6d4f8029b624d2449c8531e2f5573
Nick Girling <nickgirling76@gmail.com>
2024-12-21 16:24:01 +0000

    d

--- dave/js/script.js
+++ dave/js/script.js
@@ -1,23 +1,35 @@
 window.onscroll = function() {scrollFunction()};
-const nav = document.getElementById("nav");
+const nav = document.getElementsByClassName("nav-bg");
 const navlinks = document.getElementsByClassName("nav-link");
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    nav.style.fontSize = "25px";
-    nav.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
-    nav.style.transition = "0.4s";
-    nav.style.boxShadow = "0 4px 2px -2px rgba(0, 0, 0, 0.4)";
-    nav.style.borderRadius = "0 0 10px 10px";
-    nav.style.padding = "10px 0";
+    for (let i = 0; i < nav.length; i++) {
+        nav[i].style.fontSize = "25px";
+        nav[i].style.backgroundColor = "rgba(255, 255, 255, 0.6)";
+        nav[i].style.transition = "0.4s";
+        nav[i].style.boxShadow = "0 4px 2px -2px rgba(0, 0, 0, 0.4)";
+        nav[i].style.borderRadius = "0 10px 10px 0";
+        nav[i].style.padding = "0 10px 10px 10px";
+
+
+    if (window.innerWidth < 961) {
+      nav[i].style.fontSize = "20px";
+      nav[i].style.padding = "5px 0";
+      nav[i].style.backgroundColor = "transparent";
+      nav[i].style.boxShadow = "none";
+    }
+    }
     for (let i = 0; i < navlinks.length; i++) {
       navlinks[i].style.color = "black";
     }
   } else {
-    nav.style.backgroundColor = "transparent";
-    nav.style.fontSize = "30px";
-    nav.style.boxShadow = "none";
-    for (let i = 0; i < navlinks.length; i++) {
-      navlinks[i].style.color = "#ffccff";
+    for (let i = 0; i < nav.length; i++) {
+        nav[i].style.backgroundColor = "transparent";
+        nav[i].style.fontSize = "30px";
+        nav[i].style.boxShadow = "none";
+        for (let i = 0; i < navlinks.length; i++) {
+            navlinks[i].style.color = "#ffccff";
+        }
     }
   }
 }
